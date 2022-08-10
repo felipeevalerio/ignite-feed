3f0153998b68b588f6eee4483b04517ebbffde35
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-08-09 23:40:43 -0300

    Adicionando variáveis css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
+import './global.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Teste</h1>
+    <Header />
   )
 }
 

fe6753e7cc6ae5382ce74ec84347a8ae026f4bfa
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-08-09 23:44:50 -0300

    Finalizando Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.scss';
+import igniteLogo from '../../assets/ignite-logo.svg';
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="Logo do Ignite" />
+        </header>
+    );
+};

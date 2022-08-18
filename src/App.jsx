016f175d723a21428d9a096cf5e6895e0bf5e034
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-08-17 23:09:22 -0300

    Manipulando datas e criando conteúdo

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,26 @@ import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { Post } from './components/Post/Post';
 
+import { useState } from 'react';
 import './global.scss';
 import styles from './App.module.scss';
 
-
 function App() {
+	const [posts, setPosts] = useState([{
+		author: {
+			name: "Felipe",
+			role: "WEB Developer",
+			avatarUrl: 'http://www.github.com/felipeevalerio.png'
+		},
+		publishedAt: new Date(Date.now()),
+		content: [
+			{type: 'paragraph', content: 'Teste'},
+			{type: 'paragraph', content: 'Teste2'},
+			{type: 'link', content: 'github.com/felipeevalerio'},
+
+		]
+	}]);
+
 	return (
 		<>
 		<Header />
@@ -14,7 +29,15 @@ function App() {
 		<div className={styles.wrapper}>
 			<Sidebar />
 			<main>
-				<Post />
+				{posts.map(post => {
+					return (
+						<Post 
+							author={post.author}
+							content={post.content}
+							publishedAt={post.publishedAt}
+						/>
+					);
+				})}
 			</main>
 		</div>
 		</>
